0e06d5f4e5dcd833181c43e66554ed9e657d8805
John Mathew14 <JohnMathew141105@gmail.com>
2026-04-25 15:15:13 +0530

    files was Modified and updated

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejsMate = require("ejs-mate");
 const port = 8080;
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const ExpressError = require("./utils/ExpressError");
 const url = "mongodb://localhost:27017/DevSquad";
 
 app.set("view engine", "ejs");
@@ -30,6 +31,15 @@ async function main() {
 //home route
 app.get("/", (req, res) => {
   res.render("body/home.ejs");
+});
+
+app.all("/*splat", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found"));
+});
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something Gone Wrong" } = err;
+  res.status(statusCode).render("error.ejs", { message });
 });
 
 app.listen(port, () => {
